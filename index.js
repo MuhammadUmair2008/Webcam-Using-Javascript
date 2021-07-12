76f9c593670abcb26a65e573257773f7b985f088
MUHAMMAD UMAIR <84286269+MuhammadUmair2008@users.noreply.github.com>
2021-07-12 14:11:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var video = document.querySelector("#videoElement");
+                navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUsermedia 
+                    || navigator.mozGetUserMedia || navigato.msGetUserMedia || navigator.oGetUserMedia;
+
+                    if(navigator.getUserMedia){
+                      navigator.getUserMedia({video:true}, handleVideo, videoError)      
+                    }
+
+                    function handleVideo(stream){
+                    video.srcObject = stream;
+                    video.play();
+                    }
+
+                    function videoError(e){}
